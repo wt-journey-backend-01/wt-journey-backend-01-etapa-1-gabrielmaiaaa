3d06259940068e388627e8b9baa5131d2685f72e
Gabriel Maia <gabrielmaia6743@gmail.com>
2025-07-11 18:02:21 -0300

    Adicionando campo de preenchimento e a rota contato-recebido

--- server.js
+++ server.js
@@ -4,26 +4,37 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+var user = {
+    nome: '',
+    email: '', 
+    assunto: '', 
+    mensagem: ''
+}
+
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 app.get('/sugestao', (req, res) => {
     const { nome, ingredientes }= req.query;
-    console.log(nome, ingredientes);
 
-    res.send(`<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Agradecimento</title>
-    </head>
-    <body>
-        <p>Obrigado pela sugestão ${nome}! </p>
-        <p>Os ingredientes ${ingredientes} serão avaliados! </p>
-    </body>
-    </html>`);
+    res.send(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Agradecimento</title>
+        </head>
+        <body>
+            <p>Obrigado pela sugestão ${nome}! </p>
+            <p>Os ingredientes ${ingredientes} serão avaliados! </p>
+            <a href="/">Página Inicial</a>
+        </body>
+        </html>`
+    );
 });
 
 app.get('/contato', (req, res) => {
@@ -31,27 +42,40 @@ app.get('/contato', (req, res) => {
 });
 
 app.post('/contato', (req, res) => {
-    const { nome, email, assunto, mensagem } = req.query;
-    console.log(nome, email, assunto, mensagem)
+    const { nome, email, assunto, mensagem } = req.body;
+    user.nome = nome;
+    user.email = email;
+    user.assunto = assunto;
+    user.mensagem = mensagem;
 
-    res.send(`<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Agradecimento</title>
-    </head>
-    <body>
-        <h1> Seja Bem Vindo, ${nome}! </h1>
-        <p>Email: ${email}! </p>
-        <p>Assunto: ${assunto}! </p>
-        <p>Mensagem: ${mensagem}! </p>
-    </body>
-    </html>`);
+    res.redirect('/contato-recebido')
 });
 
+app.get('/contato-recebido', (req, res) => {
+
+    res.send(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Agradecimento</title>
+        </head>
+        <body>
+            <h1>Nome: ${user.nome}! </h1>
+            <p>Email: ${user.email}! </p>
+            <p>Assunto: ${user.assunto}! </p>
+            <p>Mensagem: ${user.mensagem}! </p>
+        </body>
+        </html>`
+    );
+})
+
 app.get('/api/lanches', (req, res) => {
-    res.send("Hello")
+    const listaLanches = [];
+    
+    res.send("Hello");
+
 });
 
 app.listen(PORT, () => {
