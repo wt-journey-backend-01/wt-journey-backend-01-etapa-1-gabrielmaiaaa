aa5f60ac2fb4de25323e1701dedcc39218d2a0f2
Gabriel Maia <gabrielmaia6743@gmail.com>
2025-07-12 15:23:22 -0300

    Adicionado a pagina 404, melhoria no css

--- server.js
+++ server.js
@@ -4,12 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-var user = {
-    nome: '',
-    email: '', 
-    assunto: '', 
-    mensagem: ''
-}
+var user = null;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
@@ -19,7 +14,11 @@ app.get('/', (req, res) => {
 });
 
 app.get('/sugestao', (req, res) => {
-    const { nome, ingredientes }= req.query;
+    const { nome, ingredientes } = req.query;
+
+    if(!nome) {
+        res.redirect('/not-found');       
+    }
 
     res.send(`
         <!DOCTYPE html>
@@ -28,10 +27,16 @@ app.get('/sugestao', (req, res) => {
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Agradecimento</title>
+            <link rel="stylesheet" href="css/style.css">
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Winky+Rough:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
         </head>
         <body>
-            <p>Obrigado pela sugestão ${nome}! </p>
-            <p>Os ingredientes ${ingredientes} serão avaliados! </p>
+            <div class="cardAgradecimento">
+                <h2>Obrigado pela sugestão, ${nome}! </h2>
+                <p>Os ingredientes ${ingredientes} serão avaliados! </p>
+            </div>
             <a href="/">Página Inicial</a>
         </body>
         </html>`
@@ -43,31 +48,37 @@ app.get('/contato', (req, res) => {
 });
 
 app.post('/contato', (req, res) => {
-    const { nome, email, assunto, mensagem } = req.body;
-    
-    user.nome = nome;
-    user.email = email;
-    user.assunto = assunto;
-    user.mensagem = mensagem;
+    user = req.body;
 
-    res.status(200).redirect('/contato-recebido');
+    res.redirect('/contato-recebido');
 });
 
 app.get('/contato-recebido', (req, res) => {
+    if(!user) {
+        res.redirect('/not-found');
+    }
+    
+    const { nome, email, assunto, mensagem } = user
 
-    res.send(`
+    res.status(200).send(`
         <!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Agradecimento</title>
+            <link rel="stylesheet" href="css/style.css">
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Winky+Rough:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
         </head>
         <body>
-            <p>Nome: ${user.nome}! </p>
-            <p>Email: ${user.email}! </p>
-            <p>Assunto: ${user.assunto}! </p>
-            <p>Mensagem: ${user.mensagem}! </p>
+            <div class="cardAgradecimento">
+                <h2>Obrigado pelo contato, ${nome}.</h2>
+                <p>Email: ${email}</p>
+                <p>Assunto: ${assunto}.</p>
+                <p>Mensagem: ${mensagem}.</p>
+            </div>
             <a href="/">Página Inicial</a>
         </body>
         </html>`
@@ -87,6 +98,10 @@ app.get('/api/lanches', (req, res) => {
     
     res.json(listaLanches);
 });
+
+app.get('/not-found', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'public/404.html'));
+})
 
 app.listen(PORT, () => {
     console.log(`Servidor da DevBurger rodando em localhost:${PORT}`);
